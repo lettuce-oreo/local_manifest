63a1bdf4d23317f2e0b064ffb6a1eede676d9f61
HARSH <48650798+hpnightowl@users.noreply.github.com>
2019-05-18 16:57:45 +0530

    Create lettuce.sh

---
+++ lettuce.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "======================Lettuce  Repo===================================="
+git clone https://github.com/lettuce-oreo/proprietary_vendor_yu.git vendor/yu
+git clone https://github.com/lettuce-oreo/android_kernel_cyanogen_msm8916.git kernel/cyanogen/msm8916
+git clone https://github.com/lettuce-oreo/android_device_yu_lettuce.git device/yu/lettuce
